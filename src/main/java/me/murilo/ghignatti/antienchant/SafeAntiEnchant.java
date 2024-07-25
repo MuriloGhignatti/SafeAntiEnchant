3dfdf5ad2718ba3473a582245d5de5d59dc0e4a2
Murilo Ghignatti <muriloghignatti@gmail.com>
2024-07-25 18:46:15 -0300

    Formatting with 4 spaces and removing unused dependencies

--- src/main/java/me/murilo/ghignatti/antienchant/SafeAntiEnchant.java
+++ src/main/java/me/murilo/ghignatti/antienchant/SafeAntiEnchant.java
@@ -1,14 +1,10 @@
 package me.murilo.ghignatti.antienchant;
 
+import java.util.Objects;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.ArrayList;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 public final class SafeAntiEnchant extends JavaPlugin {
 
@@ -18,13 +14,19 @@ public final class SafeAntiEnchant extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-        Bukkit.getServer().getConsoleSender().sendMessage(Constants.PLUGIN_PREFIX + ChatColor.translateAlternateColorCodes('&', " &aLoaded"));
+        Bukkit.getServer().getConsoleSender().sendMessage(
+            Constants.PLUGIN_PREFIX +
+            ChatColor.translateAlternateColorCodes('&', " &aLoaded"));
         generateConfig();
         blockedMaterialsHandler = new BlockedMaterialsHandler(config);
-        getServer().getPluginManager().registerEvents(new BlockEnchantEvent(blockedMaterialsHandler), this);
-        Objects.requireNonNull(getCommand("SafeAntiEnchant")).setExecutor(new SafeAntiEnchantCommand(this, config, blockedMaterialsHandler));
-        Objects.requireNonNull(getCommand("SafeAntiEnchant")).setTabCompleter(new SafeAntiEnchatTabCompleter(blockedMaterialsHandler));
-
+        getServer().getPluginManager().registerEvents(
+            new BlockEnchantEvent(blockedMaterialsHandler), this);
+        Objects.requireNonNull(getCommand("SafeAntiEnchant"))
+            .setExecutor(new SafeAntiEnchantCommand(this, config,
+                                                    blockedMaterialsHandler));
+        Objects.requireNonNull(getCommand("SafeAntiEnchant"))
+            .setTabCompleter(
+                new SafeAntiEnchatTabCompleter(blockedMaterialsHandler));
     }
 
     @Override
@@ -32,22 +34,28 @@ public final class SafeAntiEnchant extends JavaPlugin {
         forceSaveConfig();
         config = null;
         blockedMaterialsHandler = null;
-        Bukkit.getServer().getConsoleSender().sendMessage(Constants.PLUGIN_PREFIX + ChatColor.translateAlternateColorCodes('&', " &cUnloaded"));
+        Bukkit.getServer().getConsoleSender().sendMessage(
+            Constants.PLUGIN_PREFIX +
+            ChatColor.translateAlternateColorCodes('&', " &cUnloaded"));
     }
 
-    public void forceSaveConfig(){
-        config.set(Constants.BLOCKED_ITEMS_PATH, blockedMaterialsHandler.getBlockedMaterials());
+    public void forceSaveConfig() {
+        config.set(Constants.BLOCKED_ITEMS_PATH,
+                   blockedMaterialsHandler.getBlockedMaterials());
         saveConfig();
     }
 
-    private void generateConfig(){
+    private void generateConfig() {
         saveDefaultConfig();
         config = getConfig();
         config.options().copyDefaults(true);
-        config.addDefault(Constants.BLOCKED_ITEMS_PATH, new String[]{});
-        config.addDefault(Constants.MESSAGE_ADDING_ITEM_PATH, "&aAdding &fItem(s) &b{item} &fto blocked list");
-        config.addDefault(Constants.MESSAGE_REMOVING_ITEM_PATH, "&cRemoving &fItem(s) &b{item} &fto blocked list");
-        config.addDefault(Constants.MESSAGE_LIST_BLOCKED_ITEMS, "Items &cBlocked &f: {items}");
+        config.addDefault(Constants.BLOCKED_ITEMS_PATH, new String[] {});
+        config.addDefault(Constants.MESSAGE_ADDING_ITEM_PATH,
+                          "&aAdding &fItem(s) &b{item} &fto blocked list");
+        config.addDefault(Constants.MESSAGE_REMOVING_ITEM_PATH,
+                          "&cRemoving &fItem(s) &b{item} &fto blocked list");
+        config.addDefault(Constants.MESSAGE_LIST_BLOCKED_ITEMS,
+                          "Items &cBlocked &f: {items}");
         saveConfig();
     }
 }
